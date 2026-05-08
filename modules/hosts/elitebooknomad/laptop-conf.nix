2acbee27e95535881dc5bebb1738317cc8c1e63f
Michael Zuercher <michzuerch@gmail.com>
2026-05-08 09:24:30 +0200

    formating with alejandra

--- modules/hosts/elitebooknomad/laptop-conf.nix
+++ modules/hosts/elitebooknomad/laptop-conf.nix
@@ -1,70 +1,70 @@
 {den, ...}: {
-  # define vm config aspect + user aspect
-  den.hosts.x86_64-linux.elitebook-laptop.users.michzuerch = {};
+	# define vm config aspect + user aspect
+	den.hosts.x86_64-linux.elitebook-laptop.users.michzuerch = {};
 
-  # configure vm config aspect
-  den.aspects.elitebook-laptop = {
-    # re-usable modules here
-    includes = [
-      # general
+	# configure vm config aspect
+	den.aspects.elitebook-laptop = {
+		# re-usable modules here
+		includes = [
+			# general
 
-      # system
-      den.aspects.audio
-      den.aspects.bluetooth
-      den.aspects.bootloader
-      den.aspects.email
-      den.aspects.extraPackages
-      den.aspects.firefox
-      den.aspects.fonts
-      den.aspects.gaming
-      # den.aspects.gpupassthrough
-      den.aspects.input
-      den.aspects.kernel
-      den.aspects.locale
-      den.aspects.localsend
-      den.aspects.login
-      den.aspects.networking
-      den.aspects.niri
-      den.aspects.nix
-      den.aspects.printing
-      den.aspects.snapshots
-      den.aspects.virtualization
+			# system
+			den.aspects.audio
+			den.aspects.bluetooth
+			den.aspects.bootloader
+			den.aspects.email
+			den.aspects.extraPackages
+			den.aspects.firefox
+			den.aspects.fonts
+			den.aspects.gaming
+			# den.aspects.gpupassthrough
+			den.aspects.input
+			den.aspects.kernel
+			den.aspects.locale
+			den.aspects.localsend
+			den.aspects.login
+			den.aspects.networking
+			den.aspects.niri
+			den.aspects.nix
+			den.aspects.printing
+			den.aspects.snapshots
+			den.aspects.virtualization
 
-      # hardware
-      # den.aspects.fingerprint
-      # den.aspects.powersave
-      # den.aspects.kanata
-      den.aspects.elitebook-disk
+			# hardware
+			# den.aspects.fingerprint
+			# den.aspects.powersave
+			# den.aspects.kanata
+			den.aspects.elitebook-disk
 
-      den.aspects.ld
-      den.aspects.graphics
+			den.aspects.ld
+			den.aspects.graphics
 
-      # testing
-      den.aspects.plasma
-    ];
+			# testing
+			den.aspects.plasma
+		];
 
-    # system settings
-    nixos = {
-      networking.hostName = "elitebook";
+		# system settings
+		nixos = {
+			networking.hostName = "elitebook";
 
-      # hardware config
-      boot.initrd.availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "thunderbolt"
-        "usb_storage"
-        "sd_mod"
-      ];
-      boot.initrd.kernelModules = [];
-      boot.kernelModules = ["kvm-amd"];
-      boot.extraModulePackages = [];
-      nixpkgs.hostPlatform = "x86_64-linux";
-      hardware.enableRedistributableFirmware = true;
-      hardware.cpu.amd.updateMicrocode = true;
-    };
+			# hardware config
+			boot.initrd.availableKernelModules = [
+				"nvme"
+				"xhci_pci"
+				"thunderbolt"
+				"usb_storage"
+				"sd_mod"
+			];
+			boot.initrd.kernelModules = [];
+			boot.kernelModules = ["kvm-amd"];
+			boot.extraModulePackages = [];
+			nixpkgs.hostPlatform = "x86_64-linux";
+			hardware.enableRedistributableFirmware = true;
+			hardware.cpu.amd.updateMicrocode = true;
+		};
 
-    # home settings for all users on this system
-    homeManager = {
-    };
-  };
+		# home settings for all users on this system
+		homeManager = {
+		};
+	};
 }
